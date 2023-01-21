28c93ab5ebf346f100dbe15e63102443df0c45d1
Rizky Pratama <hnaura89@gmail.com>
2023-01-21 09:13:28 +0700

    fix button scroll on popular jobs section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,31 @@
-function Card() {
+function Card({datas}) {
+  const {svg,company,jobs,desk,category} = datas
   return (
-    <div className="group p-6 w-72 flex-none flex flex-col gap-5 rounded-2xl border border-[#E2E3E8] hover:bg-primary transition-all">
+    <div className="group p-6 w-72 flex-none flex flex-col gap-5 rounded-2xl border border-[#E2E3E8] bg-[#F9FAFB] hover:bg-primary transition-all z-10 snap-center">
       <div className="flex gap-2 justify-items-center group-hover:text-white">
         <div className="flex-0">
           <object
-            className="w-14 p-2 bg-[#fff] rounded-2xl shadow-lg shadow-grey/20"
-            data="/src/assets/logo/whatsapp.svg"
+            className="w-14 aspect-square object-center p-2 bg-[#fff] rounded-2xl shadow-lg shadow-grey/20"
+            data={`/src/assets/logo/${svg}.svg`}
             type="image/svg+xml"
           ></object>
         </div>
         <div className="flex-auto group-hover:text-white">
-          <h3 className="text-2xl font-bold">Spotify</h3>
+          <h3 className="text-2xl font-bold capitalize">{company}</h3>
           <span className="text-sm font-normal text-grey-50 group-hover:text-white">
-            28 Avaliable job
+            {jobs} Avaliable job
           </span>
         </div>
       </div>
       <p className="text-sm font-normal text-grey-50 group-hover:text-white">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!
+        {desk}
       </p>
       <div className="flex gap-1 text-sm font-semibold text-white">
-        <span className="px-3 py-1 rounded-md bg-primary group-hover:bg-secondary-50/40">
-          Remote
-        </span>
-        <span className="px-3 py-1 rounded-md bg-primary group-hover:bg-secondary-50/40">
-          Full Time
-        </span>
-        <span className="px-3 py-1 rounded-md bg-primary group-hover:bg-secondary-50/40">
-          WFH
-        </span>
+        {category.map((data) => (
+          <span className="px-3 py-1 rounded-md bg-primary group-hover:bg-secondary-50/40">
+            {data}
+          </span>
+        ))}
       </div>
     </div>
   );
--- src/components/PopularJobs.jsx
+++ src/components/PopularJobs.jsx
@@ -1,32 +1,99 @@
 import { Icon } from "@iconify/react";
+import { useRef } from "react";
 import Card from "./Card";
 
 function PopularJobs() {
+  const scroller = useRef(null);
+
+  function handlerScroll(dir){
+    const positionScroller = scroller.current.scrollLeft;
+    if(dir){
+      scroller.current.scroll({top:0,left:positionScroller+150,behavior:"smooth"})
+    }else{
+      scroller.current.scroll({top:0,left:positionScroller-150,behavior:"smooth"})
+    }
+  }
   return (
     <section className="bg-[#F9FAFB]">
-      <div className="container">
-        <div className="py-20 flex flex-col gap-10">
-          <div className="flex justify-between items-center">
-            <h2 className="text-4xl text-black font-bold">
-              Most very <span className="text-primary">popular jobs</span>
-            </h2>
-            <div className="flex gap-3">
-              <button className="py-1 px-2 aspect-square rounded-full text-grey-50 transition-all bg-white-50 hover:text-white hover:bg-primary hover:shadow-lg hover:shadow-primary/40">
-                <Icon icon="heroicons:arrow-long-left-solid" width="45" />
-              </button>
-              <button className="py-1 px-2 aspect-square rounded-full text-grey-50 transition-all bg-white-50 hover:text-white hover:bg-primary hover:shadow-lg hover:shadow-primary/40">
-                <Icon icon="heroicons:arrow-long-right-solid" width="45" />
-              </button>
+      <div className="relative h-full max-w-screen-2xl mx-auto">
+        <div className="container">
+          <div className="py-20 flex flex-col gap-10">
+            <div className="flex justify-between items-center">
+              <h2 className="text-4xl text-black font-bold">
+                Most very <span className="text-primary">popular jobs</span>
+              </h2>
+              <div className="flex gap-3">
+                <button
+                  className="py-1 px-2 aspect-square rounded-full text-grey-50 transition-all bg-white-50 hover:text-white hover:bg-primary hover:shadow-lg hover:shadow-primary/40"
+                  onClick={() => handlerScroll()}
+                >
+                  <Icon icon="heroicons:arrow-long-left-solid" width="45" />
+                </button>
+                <button
+                  className="py-1 px-2 aspect-square rounded-full text-grey-50 transition-all bg-white-50 hover:text-white hover:bg-primary hover:shadow-lg hover:shadow-primary/40"
+                  onClick={() => handlerScroll(1)}
+                >
+                  <Icon icon="heroicons:arrow-long-right-solid" width="45" />
+                </button>
+              </div>
+            </div>
+            <div
+              ref={scroller}
+              className="scroller flex flex-nowrap gap-5 overflow-x-auto snap-mandatory snap-x"
+            >
+              <Card
+                datas={{
+                  svg: "spotify",
+                  company: "spotify",
+                  jobs: 28,
+                  category: ["Remote", "Full Time", "WFH"],
+                  desk: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!",
+                }}
+              />
+              <Card
+                datas={{
+                  svg: "google",
+                  company: "google inc",
+                  jobs: 15,
+                  category: ["Remote", "Full Time"],
+                  desk: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!",
+                }}
+              />
+              <Card
+                datas={{
+                  svg: "meta",
+                  company: "meta inc",
+                  jobs: 28,
+                  category: ["Remote", "Full Time", "WFH"],
+                  desk: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!",
+                }}
+              />
+              <Card
+                datas={{
+                  svg: "tesla",
+                  company: "tesla",
+                  jobs: 25,
+                  category: ["Remote", "Full Time", "WFH"],
+                  desk: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!",
+                }}
+              />
+              <Card
+                datas={{
+                  svg: "agola",
+                  company: "agola",
+                  jobs: 31,
+                  category: ["Remote", "WFH"],
+                  desk: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, earum!",
+                }}
+              />
             </div>
           </div>
-          <div className="flex flex-nowrap gap-5 overflow-x-auto">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
         </div>
+        <img
+          className="absolute bottom-0 left-0 h-60 "
+          src="/src/assets/background/lines.svg"
+          alt="lines"
+        />
       </div>
     </section>
   );
