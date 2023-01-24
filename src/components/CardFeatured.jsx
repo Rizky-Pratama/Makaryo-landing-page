59c1b41b4207cb79fcf402e57cee11ef95af2938
Muhammad Rizky Pratama <73880696+Rizky-Pratama@users.noreply.github.com>
2023-01-24 03:56:01 +0000

    Final FeaturedJobs Section

--- src/components/CardFeatured.jsx
+++ src/components/CardFeatured.jsx
@@ -1,30 +1,27 @@
-function CardFeatured() {
+function CardFeatured({ labels, svg, job, location, desk, hours, price }) {
   return (
-    <div className="px-6 pt-8 pb-10 w-[361px] flex flex-col gap-5 border border-[#E2E3E8] rounded-3xl">
+    <div className="px-6 pt-8 pb-10 w-[361px] flex flex-col gap-5 border border-[#E2E3E8] rounded-3xl bg-white z-50">
       <div className="flex gap-2 text-white font-medium capitalize">
-        <span className="px-3 py-1 rounded-md bg-primary">
-          remote
-        </span>
-        <span className="px-3 py-1 rounded-md bg-primary">
-          full time
-        </span>
+        {labels?.map((label) => (
+          <span className="px-3 py-1 rounded-md bg-primary">{label}</span>
+        ))}
       </div>
       <div>
         <div className="flex gap-3">
           <object
             className="w-14 aspect-square object-center p-3 bg-[#fff] rounded-2xl shadow-lg shadow-grey/20"
-            data="/logo/google.svg"
+            data={`/logo/${svg}.svg`}
             type="image/svg+xml"
           ></object>
           <div className="flex flex-col gap-1">
-            <h3 className="text-xl font-semibold">Junior Vidiographer</h3>
+            <h3 className="text-xl font-semibold">{job}</h3>
             <div className="flex items-center gap-2">
               <object
                 className="text-grey fill-grey"
                 data="/icons/location.svg"
                 type="image/svg+xml"
               ></object>
-              <p className="text-grey-50 text-sm">Tebet, South Jakarta</p>
+              <p className="text-grey-50 text-sm">{location}</p>
             </div>
           </div>
         </div>
@@ -36,11 +33,11 @@ function CardFeatured() {
         <div className="flex gap-4">
           <div className="flex gap-2">
             <object data="/icons/clock.svg" type="image/svg+xml"></object>
-            <span>1 hours ago</span>
+            <span>{hours} hours ago</span>
           </div>
           <div className="flex gap-2">
             <object data="/icons/money.svg" type="image/svg+xml"></object>
-            <span>$10K - 110K</span>
+            <span>{price}</span>
           </div>
         </div>
         <button className="relative py-2 text-lg text-white font-medium rounded-xl bg-primary shadow-lg shadow-primary/30 hover:ring">
--- src/layouts/FeaturedJobs.jsx
+++ src/layouts/FeaturedJobs.jsx
@@ -3,28 +3,88 @@ import CardFeatured from "../components/CardFeatured";
 function FeaturedJobs() {
   return (
     <section className="bg-white">
-      <div className="container">
-        <div className="py-16 flex flex-col gap-12 text-black">
-          <div className="flex flex-col text-center gap-3">
-            <h1 className="text-4xl text-black font-bold">
-              Featured <span className="text-primary">jobs offer</span>
-            </h1>
-            <p className="text-sm text-grey">
-              Some companies that are opening vacancies and are well-paid!
-            </p>
-          </div>
-          <div className="flex flex-wrap gap-x-8 gap-y-12 justify-center">
-            <CardFeatured />
-            <CardFeatured />
-            <CardFeatured />
-            <CardFeatured />
-            <CardFeatured />
-            <CardFeatured />
+      <div className="relative mx-auto max-w-screen-2xl">
+        <div className="container">
+          <div className="py-16 flex flex-col gap-12 items-center text-black">
+            <div className="flex flex-col text-center gap-3">
+              <h1 className="text-4xl text-black font-bold">
+                Featured <span className="text-primary">jobs offer</span>
+              </h1>
+              <p className="text-sm text-grey">
+                Some companies that are opening vacancies and are well-paid!
+              </p>
+            </div>
+            <div className="flex flex-wrap gap-x-8 gap-y-12 justify-center">
+              <CardFeatured
+                labels={["remote", "full time"]}
+                svg="google"
+                job="Junior Videographer"
+                location="Tebet, South Jakarta"
+                hours="1"
+                price="$10K - 110K"
+              />
+              <CardFeatured
+                labels={["remote", "full time"]}
+                svg="linkedln"
+                job="Full Stack Dev"
+                location="Toronto, USA"
+                hours="2"
+                price="$10K - 90K"
+              />
+              <CardFeatured
+                labels={["remote", "full time"]}
+                svg="doll"
+                job="Jr. Product Designer"
+                location="Bandung, West Java"
+                hours="3"
+                price="$10K - 110K"
+              />
+              <CardFeatured
+                labels={["remote", "full time"]}
+                svg="airbnb"
+                job="Jr. UX Researcher"
+                location="Semarang, Central Java"
+                hours="4"
+                price="$10K - 140K"
+              />
+              <CardFeatured
+                labels={["remote", "full time"]}
+                svg="dropbox"
+                job="Sr. UX Designer"
+                location="New York, USA"
+                hours="5"
+                price="$10K - 220K"
+              />
+              <CardFeatured
+                labels={["remote", "freelance"]}
+                svg="gitlab"
+                job="Jr. Content Creator"
+                location="Bandung, West Java"
+                hours="6"
+                price="$50K - 220K"
+              />
+            </div>
+            <button className="w-[19rem] py-3 flex gap-3 justify-center items-center text-base font-semibold text-primary rounded-xl outline outline-2">
+              <span>Find More Jobs</span>
+              <object data="/icons/arrow-right-short.svg"></object>
+            </button>
           </div>
         </div>
+        <object
+          className="absolute bottom-0 left-0"
+          data="/background/lines.svg"
+        ></object>
+        <object
+          className="absolute bottom-0 right-0 scale-x-[-1]"
+          data="/background/lines.svg"
+        ></object>
+        <object
+          className="absolute top-0 right-0"
+          data="/background/start.svg"
+        ></object>
       </div>
     </section>
   );
 }
 
-export default FeaturedJobs
+export default FeaturedJobs;
