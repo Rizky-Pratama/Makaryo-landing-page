d90ebca239f5200f074c7ea7f2a45c57d8bf31f3
Muhammad Rizky Pratama <73880696+Rizky-Pratama@users.noreply.github.com>
2023-01-27 03:03:26 +0000

    Update Footer

--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,15 +1,80 @@
+const navs = [
+  {
+    head: "About Us",
+    links: [
+      { link: "company", href: "#" },
+      { link: "social media", href: "#" },
+      { link: "careers", href: "#" },
+      { link: "jobs", href: "#" },
+    ],
+  },
+  {
+    head: "Find Jobs",
+    links: [
+      { link: "login", href: "#" },
+      { link: "register", href: "#" },
+      { link: "customer service", href: "#" },
+      { link: "online chat", href: "#" },
+    ],
+  },
+  {
+    head: "FAQ",
+    links: [
+      { link: "account", href: "#" },
+      { link: "register", href: "#" },
+      { link: "apply jobs", href: "#" },
+      { link: "download app", href: "#" },
+    ],
+  },
+  {
+    head: "Blog",
+    links: [
+      { link: "link 1", href: "#" },
+      { link: "link 2", href: "#" },
+      { link: "link 3", href: "#" },
+      { link: "link 4", href: "#" },
+    ],
+  },
+  {
+    head: "Social Media",
+    links: [
+      { link: "instagram", href: "#" },
+      { link: "twitter", href: "#" },
+      { link: "facebook", href: "#" },
+      { link: "linked", href: "#" },
+      { link: "youtube", href: "#" },
+    ],
+  },
+  {
+    head: "Contact",
+    links: [
+      { link: "(021) 8989 6788", href: "#" },
+      { link: "makaryo@gmail.com", href: "#" },
+    ],
+  },
+];
 function NavFooter() {
   return (
-    <nav className="grid gri">
-        <ul className="flex flex-col gap-5">
-            <li><span className="text-lg font-semibold">About Us</span></li>
-            <li><a className="text-sm font-normal" href="#">Company</a></li>
-            <li><a className="text-sm font-normal" href="#">Social Media</a></li>
-            <li><a className="text-sm font-normal" href="#">Careers</a></li>
-            <li><a className="text-sm font-normal" href="#">Jobs</a></li>
+    <nav className="w-full flex justify-evenly flex-wrap gap-y-7">
+      {navs?.map((nav) => (
+        <ul className="flex-none basis-40 flex flex-col gap-5">
+          <li>
+            <span className="text-lg font-semibold">{nav.head}</span>
+          </li>
+          {nav.links.map((item) => (
+            <li>
+              <a
+                className="text-sm font-normal capitalize hover:underline underline-offset-4 transition-all"
+                href={item.href}
+              >
+                {item.link}
+              </a>
+            </li>
+          ))}
         </ul>
+      ))}
     </nav>
-  )
+  );
 }
 
-export default NavFooter
+export default NavFooter;
--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -6,10 +6,15 @@ function Footer() {
     <footer className="bg-black text-white">
         <div>
             <div className="container">
-                <div className="pt-24 pb-10 flex flex-col">
-                    <div className="flex flex-col items-center gap-24">
+                <div className="pt-24 pb-10 flex flex-col divide-y-2">
+                    <div className="pb-5 flex flex-col items-center gap-24">
                         <Download/>
                         <NavFooter/>
+                    </div>
+                    <div className="pt-5 px-16 flex items-center justify-between">
+                        <span>Makaryo</span>
+                        <span>Instagram Facebook</span>
+                        <p className="text-sm font-normal">@2023 Makaryo. All Rights Reserved</p>
                     </div>
                 </div>
             </div>
