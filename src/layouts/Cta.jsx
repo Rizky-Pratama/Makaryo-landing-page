ac3f32a8230a13f5392b0a703f65c5fcc3415c92
Muhammad Rizky Pratama <73880696+Rizky-Pratama@users.noreply.github.com>
2023-01-31 06:29:15 +0000

    Add Responsif Cta Section

--- src/layouts/Cta.jsx
+++ src/layouts/Cta.jsx
@@ -1,26 +1,26 @@
 function Cta() {
   return (
-    <section className="p-10 bg-[#F9FAFB]">
-      <div className="mx-auto h-[50rem] max-w-screen-2xl bg-[url('/Frame.png')] bg-cover bg-center bg-no-repeat overflow-hidden">
+    <section className="lg:p-10 bg-[#F9FAFB] bg-[url('/Frame.png')] bg-cover bg-center bg-no-repeat lg:bg-none">
+      <div className="mx-auto lg:h-[50rem] max-w-screen-2xl lg:bg-[url('/Frame.png')] lg:bg-cover lg:bg-center lg:bg-no-repeat  overflow-hidden">
         <div className="container">
-          <div className="flex text-white gap-5 items-center justify-center">
-            <div className="w-[32rem] flex flex-col gap-5">
-              <h1 className="text-5xl font-semibold italic">
+          <div className="pt-10 lg:pt-0 flex flex-col lg:flex-row text-white lg:gap-5 items-center justify-center">
+            <div className="lg:w-[32rem] flex flex-col gap-5">
+              <h1 className="text-3xl lg:text-5xl font-semibold italic">
                 Download Makaryo right now and achieve your dream job!
               </h1>
-              <p className="w-3/4 text-sm">
+              <p className="w-3/4 lg:text-sm">
                 Get all the information about all job vacancies from all over
                 the world by applying for jobs hassle-free with one click using
                 your gadget.
               </p>
-              <div className="w-36 flex gap-2">
+              <div className="w-32 lg:w-36 flex gap-2">
                 <img src="/app-store.png" alt="App Store" />
                 <img src="/google-play.png" alt="Google Play" />
               </div>
             </div>
-            <div className="">
+            <div>
               <img
-                className="w-[40rem] translate-y-[10%]"
+                className="lg:w-[40rem] translate-y-[10%]"
                 src="/mockup/cta.svg"
                 alt="CTA"
               />
